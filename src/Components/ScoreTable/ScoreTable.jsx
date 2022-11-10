ccf803fe2296c319f5e1ebd3b2dfb2511e871a2c
jshprintz <jshprintz@gmail.com>
2022-11-10 12:21:53 -0800

    before it bugs out

--- src/Components/ScoreTable/ScoreTable.jsx
+++ src/Components/ScoreTable/ScoreTable.jsx
@@ -1,31 +1,32 @@
 import React, { useEffect } from "react";
+import ScoreLine from "../ScoreLine/ScoreLine";
 import "./ScoreTable.css";
 
 export default function ScoreTable() {
-    const scoreList = [];
+  const scoreList = [];
 
-    useEffect(() => {
-        getInitialScores();
-    })
+  useEffect(() => {
+    getInitialScores();
+  });
 
-
-    function getInitialScores() {
-        while (scoreList.length < 20 ){
-            scoreList.push(randomScore);
-        }
+  function getInitialScores() {
+    while (scoreList.length < 20) {
+      scoreList.push(randomScore);
     }
+  }
 
-
-    function randomScore() {
-        return Math.floor(Math.random() * (20000 - 10000 +1) + 10000);
-    }
-
-
+  function randomScore() {
+    return Math.floor(Math.random() * (20000 - 10000 + 1) + 10000);
+  }
 
   return (
     <div id="score-table-container">
       <div id="score-table">
-
+        {scoreList.map((score) => {
+            return (
+                <ScoreLine score={score} />
+            )
+        })}
       </div>
     </div>
   );
