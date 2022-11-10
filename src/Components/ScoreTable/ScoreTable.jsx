121a6f0c18f033eda42e549e668fe2deae4121f3
jshprintz <jshprintz@gmail.com>
2022-11-10 13:21:20 -0800

    numbers are now displaying correctly

--- src/Components/ScoreTable/ScoreTable.jsx
+++ src/Components/ScoreTable/ScoreTable.jsx
@@ -1,26 +1,18 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import ScoreLine from "../ScoreLine/ScoreLine";
 import "./ScoreTable.css";
 
 export default function ScoreTable() {
-  //const scoreList = [randomScore()];
   const [scoreListState, setScoreListState] = useState([]);
 
-  useEffect(() => {
-    console.log("useeffect launched")
-    getInitialScores();
-  });
+  getInitialScores();
 
   function getInitialScores() {
-    console.log("initial score launched")
-
-    while (scoreListState.length < 20) {
+   if (scoreListState.length < 20) {
          setScoreListState(oldState => [...oldState, randomScore()]) 
          console.log(scoreListState)
     }
   }
-
-
 
   function randomScore() {
     return Math.floor(Math.random() * (20000 - 10000 + 1) + 10000);
