5623056437de2ed8b96a3b8d6706f6d0e185327e
jshprintz <jshprintz@gmail.com>
2022-11-10 14:00:56 -0800

    ranks and scores both working and in place on table

--- src/Components/ScoreLine/ScoreLine.jsx
+++ src/Components/ScoreLine/ScoreLine.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import "./ScoreLine.css";
 
-export default function ScoreLine({score, rank}) {
+export default function ScoreLine({ score, rank }) {
   return (
     <div id="score-line-container">
-      <div id="score-line">
-        <p>{rank++} The score is {score}</p>
-      </div>
+        <div id="rank"><p>{rank++}</p></div>
+        <div id="score"><p>{score}</p></div>
     </div>
   );
 }
