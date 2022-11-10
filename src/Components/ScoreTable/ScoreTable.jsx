ece616976f1d6ae4dd9d06fa21f001b754664633
jshprintz <jshprintz@gmail.com>
2022-11-10 11:14:27 -0800

    initial score function

--- src/Components/ScoreTable/ScoreTable.jsx
+++ src/Components/ScoreTable/ScoreTable.jsx
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./ScoreTable.css";
 
 export default function ScoreTable() {
+    const scoreList = [];
+
+    useEffect(() => {
+        getInitialScores();
+    })
+
+
+    function getInitialScores() {
+        while (scoreList.length < 20 ){
+            scoreList.push(randomScore);
+        }
+    }
+
+
+    function randomScore() {
+        return Math.floor(Math.random() * (20000 - 10000 +1) + 10000);
+    }
+
+
+
   return (
     <div id="score-table-container">
-      <div id="score-table"></div>
+      <div id="score-table">
+
+      </div>
     </div>
   );
 }
