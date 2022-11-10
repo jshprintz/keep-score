9d5df73ce90959ff02f09932c896212ac9a81b82
jshprintz <jshprintz@gmail.com>
2022-11-10 14:28:14 -0800

    random name generator working

--- src/Components/ScoreLine/ScoreLine.jsx
+++ src/Components/ScoreLine/ScoreLine.jsx
@@ -4,8 +4,9 @@ import "./ScoreLine.css";
 export default function ScoreLine({ score, rank }) {
   return (
     <div id="score-line-container">
-        <div id="rank"><p>{rank++}</p></div>
-        <div id="score"><p>{score}</p></div>
+        <div id="rank"><p>{rank}</p></div>
+        <div id="name"><p>{score.name}</p></div>
+        <div id="score"><p>{score.score}</p></div>
     </div>
   );
 }
