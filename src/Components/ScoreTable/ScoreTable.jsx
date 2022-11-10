edb0c5b3937160d903264a9ace375788584e69c9
jshprintz <jshprintz@gmail.com>
2022-11-10 10:58:20 -0800

    wireframing layout

---
+++ src/Components/ScoreTable/ScoreTable.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./ScoreTable.css";
+
+export default function ScoreTable() {
+  return (
+    <div id="score-table-container">
+      <div id="score-table"></div>
+    </div>
+  );
+}
