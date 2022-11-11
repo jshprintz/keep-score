fec4e5dc37af0f305ac9dc65af3ee5ab4b38a4fd
jshprintz <jshprintz@gmail.com>
2022-11-10 19:16:58 -0800

    icons are loaded and colored

--- src/Components/ScoreLine/ScoreLine.jsx
+++ src/Components/ScoreLine/ScoreLine.jsx
@@ -1,11 +1,22 @@
 import React from "react";
+import { ImArrowDown, ImArrowUp } from 'react-icons/im';
 import "./ScoreLine.css";
 
 export default function ScoreLine({ score, rank }) {
+    const rankChange = (rank - score.rankDiff -1) * -1;
+
+
+
   return (
     <div id="score-line-container">
         <div id="player-rank"><p>{rank}</p></div>
-        <div id="player-rank-diff"><p>{(rank - score.rankDiff -1) * -1}</p></div>
+        { score.scoreDiff ? (
+        <div id="player-rank-diff-icon">{ rankChange < 0 ? <ImArrowDown color="red" /> : rankChange > 0 ? <ImArrowUp color="green" /> : null} </div>
+        ) : null }
+        { score.scoreDiff ? (
+        <div id="player-rank-diff"><p>{rankChange}</p></div>
+        ) : null }
+        
         {/* <div ><img src={score.avatar} alt="profile" id="player-image"/></div> */}
         <div id="player-name"><p>{score.name}</p></div>
         { score.scoreDiff ? (
