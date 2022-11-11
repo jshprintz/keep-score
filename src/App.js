c3c82fade496d332d55a78871228004ae675007d
jshprintz <jshprintz@gmail.com>
2022-11-10 18:33:47 -0800

    math works

--- src/App.js
+++ src/App.js
@@ -1,12 +1,52 @@
+import React, { useState } from "react";
 import Headline from "./Components/Headline/Headline";
 import ScoreTable from "./Components/ScoreTable/ScoreTable";
+import { faker } from "@faker-js/faker";
 import './App.css';
 
 function App() {
+  const [scoreListState, setScoreListState] = useState([]);
+
+  init();
+
+    // Initialize program
+    function init() {
+      // Init get scores/users
+      if (scoreListState.length < 20) {
+        setScoreListState((oldState) => [...oldState, newUser()]);
+      } else {
+       sortList()
+      }
+    }
+
+     // Sort the list
+  function sortList() {
+    const sorted = [...scoreListState].sort((a, b) => {
+        return b.score - a.score;
+        });
+
+    return sorted;
+  }
+
+  // Init new user
+  function newUser() {
+    const newUser = {
+      name: faker.name.fullName(),
+      score: randomNumber(100, 25),
+      scoreDiff: null
+    };
+    return newUser;
+  }
+
+  // Init random score
+  function randomNumber(max, min) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
   return (
     <>
       <Headline />
-      <ScoreTable />
+      <ScoreTable sortList={sortList} randNum={randomNumber} setScoreListState={setScoreListState} />
     </>
   );
 }
--- src/Components/ScoreTable/ScoreTable.jsx
+++ src/Components/ScoreTable/ScoreTable.jsx
@@ -1,63 +1,27 @@
 import React, { useState, useEffect } from "react";
-import { faker } from "@faker-js/faker";
 import ScoreLine from "../ScoreLine/ScoreLine";
 import "./ScoreTable.css";
 
-export default function ScoreTable() {
-  const [scoreListState, setScoreListState] = useState([]);
-  let sorted = scoreListState;
-
-  init();
+export default function ScoreTable({sortList, setScoreListState, randNum}) {
+  const sorted = sortList()
 
   useEffect(() => {
-    setInterval(simulateChanges, 5000);
+    const interval = setInterval(simulateChanges, 5000);
+    console.log(sorted)
+
+    return () => clearInterval(interval);
   });
 
-  // Initialize program
-  function init() {
-    // Init get scores/users
-    if (scoreListState.length < 20) {
-      setScoreListState((oldState) => [...oldState, newUser()]);
-    } else {
-     sortList()
-    }
-  }
-
   function simulateChanges() {
-    sortList();
     setScoreListState(sorted);
 
     // Handicap
     for (let i=0; i<sorted.length; i++) {
-        const scoreChange = (randomNumber(i+10, 1));
+        const scoreChange = (randNum(i+10, 1));
         sorted[i].score += scoreChange;
         sorted[i].scoreDiff = scoreChange;
     }
   }
-
-  // Sort the list
-  function sortList() {
-    sorted = [...scoreListState].sort((a, b) => {
-        return b.score - a.score;
-        });
-  }
-
-  // Init new user
-  function newUser() {
-    const newUser = {
-      name: faker.name.fullName(),
-      score: randomNumber(100, 25),
-      scoreDiff: null
-    };
-    return newUser;
-  }
-
-  // Init random score
-  function randomNumber(max, min) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  }
-
-  
 
   return (
     <div id="score-table-container">
