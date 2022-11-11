0058fe601b652cfb182129e6c8bc76b3e87081c6
jshprintz <jshprintz@gmail.com>
2022-11-11 10:31:12 -0800

    added slight motion to scroll

--- src/Components/ScoreLine/ScoreLine.jsx
+++ src/Components/ScoreLine/ScoreLine.jsx
@@ -1,32 +1,52 @@
 import React from "react";
-import { ImArrowDown, ImArrowUp } from 'react-icons/im';
+import { motion } from "framer-motion"
+import { ImArrowDown, ImArrowUp } from "react-icons/im";
 import "./ScoreLine.css";
 
 export default function ScoreLine({ score, rank }) {
-    const rankChange = (rank - score.rankDiff -1) * -1;
-
-
+  const rankChange = (rank - score.rankDiff - 1) * -1;
 
   return (
+  <motion.div
+  initial={{ opacity: 0}}
+  whileInView={{ opacity: 1}}
+  viewport={{ once:true }}
+  >
     <div id="score-line-container">
       <div id="left-group-table">
-        <div id="player-rank"><p>{rank}</p></div>
-        { score.scoreDiff ? (
-        <div id="player-rank-diff-icon">{ rankChange < 0 ? <ImArrowDown color="red" id="arrow-down"/> : rankChange > 0 ? <ImArrowUp color="green" id="arrow-up"/> : null} </div>
-        ) : null }
-        { score.scoreDiff ? (
-        <div id="player-rank-diff"><p>{rankChange}</p></div>
-        ) : null }
+        <div id="player-rank">
+          <p>{rank}</p>
         </div>
-        {/* <div ><img src={score.avatar} alt="profile" id="player-image"/></div> */}
-        <div id="player-name"><p>{score.name}</p></div>
-        <div id="right-group-table">
-        { score.scoreDiff ? (
-
-        <div id="player-scoreDiff"><p>+ {score.scoreDiff}</p></div>
-        ) : null }
-        <div id="player-score"><p>{score.score}</p></div>
+        {score.scoreDiff ? (
+          <div id="player-rank-diff-icon">
+            {rankChange < 0 ? (
+              <ImArrowDown color="red" id="arrow-down" />
+            ) : rankChange > 0 ? (
+              <ImArrowUp color="green" id="arrow-up" />
+            ) : null}{" "}
+          </div>
+        ) : null}
+        {score.scoreDiff ? (
+          <div id="player-rank-diff">
+            <p>{rankChange}</p>
+          </div>
+        ) : null}
+      </div>
+      {/* <div ><img src={score.avatar} alt="profile" id="player-image"/></div> */}
+      <div id="player-name">
+        <p>{score.name}</p>
+      </div>
+      <div id="right-group-table">
+        {score.scoreDiff ? (
+          <div id="player-scoreDiff">
+            <p>+ {score.scoreDiff}</p>
+          </div>
+        ) : null}
+        <div id="player-score">
+          <p>{score.score}</p>
         </div>
+      </div>
     </div>
+    </motion.div>
   );
 }
