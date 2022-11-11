a0ebfe5d02f17b25ca2bc0463371413359a27308
jshprintz <jshprintz@gmail.com>
2022-11-10 18:50:17 -0800

    ranks work

--- src/Components/ScoreLine/ScoreLine.jsx
+++ src/Components/ScoreLine/ScoreLine.jsx
@@ -5,6 +5,7 @@ export default function ScoreLine({ score, rank }) {
   return (
     <div id="score-line-container">
         <div id="player-rank"><p>{rank}</p></div>
+        <div id="player-rank-diff"><p>{(rank - score.rankDiff -1) * -1}</p></div>
         {/* <div ><img src={score.avatar} alt="profile" id="player-image"/></div> */}
         <div id="player-name"><p>{score.name}</p></div>
         { score.scoreDiff ? (
